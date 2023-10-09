a53ff7819315d0de49d34be5640302cd4cbf4a4b
tajciglar <78543798+tajciglar@users.noreply.github.com>
2023-10-09 12:49:51 +0000

    Add input for player names

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
-// create first page where players can enter their names
 
-const board = document.getElementById("gameboard");
+const gameModule = (function() {
 
-const players = (name, marker) => {
-    return {name, marker}
-}
+    let player1;
+    let player2;
 
-const player1 = players("Taj")
-const player2 = players("")
+    // create first page where players can enter their names
+    const namesForm = document.getElementById("namesForm");
+
+    namesForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const firstNameInput = document.querySelector('input[name="firstName"]');
+        const secondNameInput = document.querySelector('input[name="secondName"]');
+
+        const firstNameValue = firstNameInput.value;
+        const secondNameValue = secondNameInput.value;
+
+        // create players
+        player1 = createPlayer(firstNameValue, "x")
+        player2 = createPlayer(secondNameValue, "O");
+
+        console.log(player1.name);
+        console.log(player1.marker);
+        
+        showGameBoard();
+    });
+
+    //factory function to create a player
+    function createPlayer (name, marker) {
+        return {name, marker};
+    }
+
+    function showGameBoard () {
+        const container = document.getElementById("container");
+        const form = document.querySelector(".form");
+        form.remove();
+        const board = document.createElement("div");
+        board.setAttribute("id", "gameBoard");
+        container.appendChild(board); 
+    }
+
+
+
+})();
+
+
+
+
+
+
